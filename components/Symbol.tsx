c5c36f3284e764a186e3a6a23d7441b1cc0d81f6
uemura999 <koror99espana@gmail.com>
2024-06-23 16:40:05 +0900

    add:コメントの削除

--- components/Symbol.tsx
+++ components/Symbol.tsx
@@ -5,9 +5,9 @@ import FadeIn from './FadeIn';
 const Symbol = () => {
   return (
     
-    <div className="flex justify-center items-center mt-10"> {/* Adjusted height using TailwindCSS */}
+    <div className="flex justify-center items-center mt-10"> 
     
-      <FadeIn duration={1.5} initial={{ opacity: 0, x: 0 }}> {/* Shortened initial x distance */}
+      <FadeIn duration={1.5} initial={{ opacity: 0, x: 0 }}> 
         <div className="text-6xl font-bold text-white transform hover:scale-105 transition-transform duration-300">COROLE</div> {/* TailwindCSS for styling, using inherit color */}
       </FadeIn>
       
